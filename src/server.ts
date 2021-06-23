d7c73ce888c32a8b4fe38184f1357c2c9989e20b
Viniciusalgueiro <viniciussalgueiro09@gmail.com>
2021-06-23 01:02:47 -0300

    aula 2

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,8 @@
+import "reflect-metadata" 
 import express from "express";
 
+import "./database";
+
 const app = express();
-
-app.get("/test", (request, response) => {
-    return response.send("ola mundo")
-});
-
-app.post("/test-post", (request, response) => {
-    return response.send("continue a nadar")
-})
 
 app.listen(3000, () => console.log("server is running") );
